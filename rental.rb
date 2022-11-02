54000d697eee253f9e2e9ee3a0fa95c8c0bab20a
Kehinde Onifade <onifkay@gmail.com>
2022-11-02 12:20:07 +0100

    update rental

--- rental.rb
+++ rental.rb
@@ -4,11 +4,5 @@ class Rental
 
     def initialize(date, person, book)
         @date = date
-
-        @person = person
-        person.rentals << self
-
-        @book = book
-        book.rentals << self
     end
   end
