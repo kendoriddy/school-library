15f8eb20d35039b73b86ab709e97e485f0bdb720
Anas <anas.chaabane98@gmail.com>
2022-11-10 17:22:30 +0100

    testing

---
+++ spec/rental_spec.rb
@@ -0,0 +1,22 @@
+require_relative('spec_helper')
+
+describe Rental do
+  before(:each) do
+    @person = Person.new(22, 'Ridwan')
+    @book = Book.new('War of Kings', 'Muyiwa Ige')
+  end
+
+  it 'Create rental with both person & book' do
+    rental = Rental.new('2022/08/31', @book, @person)
+    expect(@book.rentals.first).to be(rental)
+    expect(@person.rentals.first).to be(rental)
+  end
+
+  it 'Create rental with both person & book' do
+    rental = Rental.new('2022/08/31', @book, @person)
+    expect(rental.date).to eq('2022/08/31')
+  end
+end
+
+
+For rental_spec.rb
